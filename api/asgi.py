1934e4f8f3a61b307d31a47d38385c0d280b60a6
Muazu Yakub Idris <imuazyakub@gmail.com>
2025-02-09 20:28:31 +0100

    Deployment prep

--- api/asgi.py
+++ api/asgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings')
+# Set this for deployment
+settings_module = 'api.dep_settings' if 'RENDER_EXTERNAL_HOSTNAME' in os.environ else 'api.settings'
+os.environ.setdefault('DJANGO_SETTINGS_MODULE',settings_module )
 
 application = get_asgi_application()
--- api/wsgi.py
+++ api/wsgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings')
+# Set this for deployment
+settings_module = 'api.dep_settings' if 'RENDER_EXTERNAL_HOSTNAME' in os.environ else 'api.settings'
+os.environ.setdefault('DJANGO_SETTINGS_MODULE',settings_module )
 
 application = get_wsgi_application()
